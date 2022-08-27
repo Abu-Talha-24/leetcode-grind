6ed0afd39528a4cb6b16add827860bc4360546c0
Abu-Talha-24 <itsabutalha@gmail.com>
2022-08-27 23:56:06 +0530

    Time: 260 ms (76.81%), Space: 15.2 MB (56.00%) - LeetHub

--- 581-shortest-unsorted-continuous-subarray/581-shortest-unsorted-continuous-subarray.py
+++ 581-shortest-unsorted-continuous-subarray/581-shortest-unsorted-continuous-subarray.py
@@ -6,28 +6,35 @@ class Solution(object):
         """
         i = 0
         j = len(nums)-1
+        
         while i<len(nums)-1:
+            # Find First Dip
             if nums[i]<=nums[i+1]:
                 i+=1
             else:
                 break
+                
         while j>0:
+            # Find First Bump (from Right)
             if nums[j]>=nums[j-1]:
                 j-=1
             else:
                 break
-        if i==j:
+                
+        if i==j:  # If Array Sorted
             return 0
         
         unordered_subarray = nums[i:j+1]
+        
         if len(unordered_subarray)==0:
             return 0
         else:
             local_min = min(nums[i:j+1])
             local_max = max(nums[i:j+1])
-            while i>0 and nums[i-1]>local_min:
+            # Expanding the unordered sub array
+            while i > 0 and nums[i-1] > local_min:
                 i-=1
-            while j<len(nums)-1 and nums[j+1]<local_max:
+            while j < len(nums)-1 and nums[j+1] < local_max:
                 j+=1
                 
         return j-i+1
