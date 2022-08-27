54639504dac27d016b6047c602c908c9ae9a35df
Abu-Talha-24 <itsabutalha@gmail.com>
2022-08-27 22:57:45 +0530

    Time: 1518 ms (5.46%), Space: 14.1 MB (50.26%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.py
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.py
@@ -1,15 +1,12 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        
+        # Sliding Window Technique
         n = len(s)
         start = 0
         end = 0
         str_set = set()
         substr = ""
         res = ""
-        
-        if n == 0:
-            return 0
         
         while(end < n):
             
@@ -21,6 +18,7 @@ class Solution:
                     if len(substr) > len(res):
                         res = substr 
                 end += 1
+                
             elif s[end] in str_set :
                 str_set = set()
                 if len(substr) > len(res):
