77ceaa047e58330d91ff075dd00d7c35b281a601
Abu-Talha-24 <itsabutalha@gmail.com>
2022-09-26 22:05:23 +0530

    Time: 157 ms (77.52%), Space: 14.2 MB (7.84%) - LeetHub

--- 1920-build-array-from-permutation/1920-build-array-from-permutation.py
+++ 1920-build-array-from-permutation/1920-build-array-from-permutation.py
@@ -3,10 +3,13 @@ class Solution:
         
         n = len(nums)
         
-        for i in range(n):
+        for i in range(n):    # using modulo to save the ans and original num
             nums[i] = nums[i] + (n * (nums[nums[i]] % n) )
+        # nums[i] // n always gives 0 (as 0 < nums[i] < n)
+        # second_term // n gives the req answer
+        # Modulo is to get the value back in to the range when prev value is accessed
             
-        for i in range(n):
+        for i in range(n):    # retreiving the value 
             nums[i] = nums[i] // n
     
         return nums
