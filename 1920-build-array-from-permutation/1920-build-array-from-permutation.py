7ee62546d42fd0c78d2437b799e72b6ac4894914
Abu-Talha-24 <itsabutalha@gmail.com>
2022-09-27 14:07:04 +0530

    Time: 136 ms (85.28%), Space: 14.1 MB (89.58%) - LeetHub

--- 1920-build-array-from-permutation/1920-build-array-from-permutation.py
+++ 1920-build-array-from-permutation/1920-build-array-from-permutation.py
@@ -7,7 +7,7 @@ class Solution:
             nums[i] = nums[i] + (n * (nums[nums[i]] % n) )
         # nums[i] // n always gives 0 (as 0 < nums[i] < n)
         # second_term // n gives the req answer
-        # Modulo is to get the value back in to the range when prev value is accessed
+        # Modulo is to get the value back in to the range(< n) when prev value is accessed
             
         for i in range(n):    # retreiving the value 
             nums[i] = nums[i] // n
