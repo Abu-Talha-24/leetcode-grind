0bc64c0354326eb7b7bc387bf7dfd35369f2ebc2
Abu-Talha-24 <itsabutalha@gmail.com>
2022-09-27 13:46:09 +0530

    Time: 89 ms (14.24%), Space: 14.1 MB (29.46%) - LeetHub

--- 1502-can-make-arithmetic-progression-from-sequence/1502-can-make-arithmetic-progression-from-sequence.py
+++ 1502-can-make-arithmetic-progression-from-sequence/1502-can-make-arithmetic-progression-from-sequence.py
@@ -4,9 +4,7 @@ class Solution:
         
         arr.sort()
         
-        for i in range(n):
-                if i == n-2:
-                    break
+        for i in range(n-2):
                 if arr[i+1]-arr[i] != arr[i+2] - arr[i+1]:
                     return False
         
