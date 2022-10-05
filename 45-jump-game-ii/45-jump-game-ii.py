eab464ffbefa43750f723ecec401928a6e738db7
Abu-Talha-24 <itsabutalha@gmail.com>
2022-10-06 01:43:46 +0530

    Time: 350 ms (36.40%), Space: 15.1 MB (56.92%) - LeetHub

--- 45-jump-game-ii/45-jump-game-ii.py
+++ 45-jump-game-ii/45-jump-game-ii.py
@@ -11,3 +11,16 @@ class Solution:
                 curr_pos = distance
                 count+=1
         return count
+    
+        def jump2(self, nums: List[int]) -> int:
+            res = 0
+            l = r = 0
+            jumpCount = 0
+            while r < len(nums)-1:
+                maxJump = 0
+                for i in range(l, r+1):
+                    maxJump = max(maxJump, i + nums[i])
+                l = r+1
+                r = maxJump
+                jumpCount += 1
+            return jumpCount
