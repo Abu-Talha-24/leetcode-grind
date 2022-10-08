08c84fa0d399c5e03955bf29b8f63a318a5b9452
Abu-Talha-24 <itsabutalha@gmail.com>
2022-10-08 16:57:22 +0530

    Time: 209 ms (13.66%), Space: 15.5 MB (49.45%) - LeetHub

--- 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.py
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.py
@@ -1,38 +1,84 @@
 class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
+        n = len(nums)
         def first_position():
-            low, high = 0, len(nums)-1
-
-            while low <= high:
-                mid = (low + high)//2
+            l = 0
+            r = n - 1
+            
+            while (l <= r):
+                mid = l + (r - l) // 2
+                
                 if nums[mid] == target:
                     if mid > 0 and nums[mid - 1] == target:
-                        high = mid - 1
+                        r = mid - 1
                     else:
                         return mid             
-                elif nums[mid] < target:
-                    low = mid + 1
-                elif nums[mid] > target:
-                    high = mid - 1
-
+                
+                if target > nums[mid]:
+                    l = mid + 1
+                elif target < nums[mid]:
+                    r = mid - 1
+            
             return -1
-    
+        
         def last_position():
-            low, high = 0, len(nums) - 1
-
-            while low <= high:
-                mid = (low + high)//2 
-
+            l = 0
+            r = n - 1
+            
+            while (l <= r):
+                mid = l + (r-l) // 2
+                
                 if nums[mid] == target:
-                    if mid < len(nums)-1 and nums[mid+1] == target:
-                        low = mid + 1
+                    if mid < n-1 and nums[mid+1] == target:
+                        l = mid + 1
                     else:
                         return mid
-                elif nums[mid] < target:
-                    low = mid + 1
-                elif nums[mid] > target:
-                    high = mid - 1
-
+                    
+                
+                if target > nums[mid]:
+                    l = mid + 1
+                elif target < nums[mid]:
+                    r = mid - 1
+            
             return -1
-    
+        
         return [first_position(), last_position()]
+        
+        
+        
+#         def first_position():
+#             low, high = 0, len(nums)-1
+
+#             while low <= high:
+#                 mid = (low + high)//2
+#                 if nums[mid] == target:
+#                     if mid > 0 and nums[mid - 1] == target:
+#                         high = mid - 1
+#                     else:
+#                         return mid             
+#                 elif nums[mid] < target:
+#                     low = mid + 1
+#                 elif nums[mid] > target:
+#                     high = mid - 1
+
+#             return -1
+    
+#         def last_position():
+#             low, high = 0, len(nums) - 1
+
+#             while low <= high:
+#                 mid = (low + high)//2 
+
+#                 if nums[mid] == target:
+#                     if mid < len(nums)-1 and nums[mid+1] == target:
+#                         low = mid + 1
+#                     else:
+#                         return mid
+#                 elif nums[mid] < target:
+#                     low = mid + 1
+#                 elif nums[mid] > target:
+#                     high = mid - 1
+
+#             return -1
+    
+#         return [first_position(), last_position()]
