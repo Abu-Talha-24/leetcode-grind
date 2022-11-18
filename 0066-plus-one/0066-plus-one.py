047d7bff44ad770f3e9ddd917f9da46f45a0e4d5
Abu Talha <itsabutalha@gmail.com>
2022-11-19 02:01:03 +0530

    Time: 65 ms (37.33%), Space: 13.9 MB (12.20%) - LeetHub

---
+++ 0066-plus-one/0066-plus-one.py
@@ -0,0 +1,18 @@
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        
+        num = ""
+        for n in digits:
+            num += str(n)
+        
+        num = int(num)
+        
+        num += 1
+        
+        num = str(num)
+        
+        res = [x for x in num]
+
+        return res
+            
+        
