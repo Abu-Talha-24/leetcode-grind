b8ed5b99441d8365c20e5f845cb988f8da3d214f
Abu Talha <itsabutalha@gmail.com>
2022-11-28 19:51:55 +0530

    Time: 73 ms (7.28%), Space: 14 MB (15.54%) - LeetHub

--- 1046-last-stone-weight/1046-last-stone-weight.py
+++ 1046-last-stone-weight/1046-last-stone-weight.py
@@ -1,21 +1,22 @@
 class Solution:
     def lastStoneWeight(self, stones: List[int]) -> int:
         
-        # Complexity : O(n^2*log(n)) 
+        # Complexity : O(nlogn) 
         
-        while len(stones) > 1: # n
-            stones.sort()          # n logn
-            stone1 = stones.pop()
-            stone2 = stones.pop()
-            if stone1 == stone2:
-                continue
-            else:
-                if stone1 > stone2:
-                    stones.append(stone1-stone2)
-                else:
-                    stones.append(stone2-stone1)
-            
-        return stones[0] if len(stones) else len(stones)
+        stones = [-s for s in stones] # Making a max heap
+        heapq.heapify(stones)
+        
+        while len(stones) > 1:
+            first = heapq.heappop(stones)
+            second = heapq.heappop(stones)
+            # ! Opposite signs as the values in heap are -ve
+            if second > first:   #!
+                heapq.heappush(stones, first - second)  #!
+                    
+                
+        return abs(stones[0]) if len(stones) > 0 else len(stones)
+                
+        
                     
                     
             
