e0d3def2383b76b25fcb6fa1d2dd312793de9d28
Abu Talha <itsabutalha@gmail.com>
2022-11-29 06:46:25 +0530

    Time: 230 ms (42.83%), Space: 14 MB (6.51%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.py
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -1,55 +1,23 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
         
-        rows = defaultdict(set)
-        cols = defaultdict(set)
-        grids = defaultdict(set)
-        
-        n = len(board)
-        
-        for i in range(n):
-            
-            for j in range(n):
-                num = board[i][j]
-                if num == ".":
+        cols = collections.defaultdict(set)
+        rows = collections.defaultdict(set)
+        squares = collections.defaultdict(set)  # key = (r /3, c /3)
+
+        for r in range(9):
+            for c in range(9):
+                if board[r][c] == ".":
                     continue
-                if num != '.' and num in rows[i] or num in cols[j]:
-                    print(num, i, j)
+                if (
+                    board[r][c] in rows[r]
+                    or board[r][c] in cols[c]
+                    or board[r][c] in squares[(r // 3, c // 3)]
+                ):
                     return False
-                
-                rows[i].add(num)
-                cols[j].add(num)
-                
-                gr = 0
-                if i <=2:
-                    if j <= 2:
-                        gr = 1
-                    elif j <= 5:
-                        gr = 2
-                    elif j <= 8:
-                        gr = 3
-                elif i <= 5:
-                    if j <= 2:
-                        gr = 4
-                    elif j <= 5:
-                        gr = 5
-                    elif j <= 8:
-                        gr = 6
-                elif j <= 8:
-                    if j <= 2:
-                        gr = 7
-                    elif j <= 5:
-                        gr = 8
-                    elif j <= 8:
-                        gr = 9
-                
-                if num in grids[gr]:
-                    print(num, "grid", gr)
-                    return False
-                
-                grids[gr].add(num)
-                
-            
+                cols[c].add(board[r][c])
+                rows[r].add(board[r][c])
+                squares[(r // 3, c // 3)].add(board[r][c])
+
         return True
-                
-        
+
