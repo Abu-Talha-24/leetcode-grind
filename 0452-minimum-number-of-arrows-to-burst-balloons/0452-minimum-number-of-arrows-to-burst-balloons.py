2ea586a13cefc8816012981978d78d78038e0d88
Abu Talha <itsabutalha@gmail.com>
2023-01-05 16:06:23 +0530

    Time: 3155 ms (35.03%), Space: 60.4 MB (5.48%) - LeetHub

---
+++ 0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.py
@@ -0,0 +1,26 @@
+class Solution:
+    def findMinArrowShots(self, points: List[List[int]]) -> int:
+
+            
+        points.sort(key=lambda x : x[0])
+        res = []
+        prev = []
+        
+        # combine the intervals with overlapping co-ords
+        for i in range(len(points)):
+            if i == 0:
+                prev = points[0]
+                res.append(prev)
+                continue
+            if points[i][0] <= prev[1]: # has overlapping
+                pt = [points[i][0], min(prev[1], points[i][1])]
+                res[-1] = pt # replace the last one as it is overlapped
+                prev = pt
+            else:
+                prev = points[i]
+                res.append(prev)
+        
+        
+        #return the length of the points array
+        print(res)
+        return len(res)
