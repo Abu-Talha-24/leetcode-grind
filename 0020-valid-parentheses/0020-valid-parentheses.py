1728bb1772136616cb04e38873540153b3e85bf5
Abu Talha <itsabutalha@gmail.com>
2023-02-24 12:52:26 +0530

    Time: 39 ms (32.84%), Space: 13.8 MB (61.07%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -2,17 +2,19 @@ class Solution:
     def isValid(self, s: str) -> bool:
         
         hm = {")": "(",
-              "]": "[",
-              "}": "{"}
+	         "]": "[",
+	         "}": "{"}   
         
         st = []
         
         for char in s:
-            if char not in hm.keys():
+            if char not in hm:
                 st.append(char)
-            elif len(st) and hm[char] == st[-1]:
+            elif st and hm[char] == st[-1]:
                 st.pop()
             else:
                 return False
         
         return not len(st)
+                
+                
