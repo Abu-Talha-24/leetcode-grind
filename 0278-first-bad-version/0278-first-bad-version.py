97b384d4cd81c978fafead155f6c27d3b411416e
Abu Talha <itsabutalha@gmail.com>
2023-02-26 02:00:04 +0530

    Time: 27 ms (87.85%), Space: 13.8 MB (51.26%) - LeetHub

---
+++ 0278-first-bad-version/0278-first-bad-version.py
@@ -0,0 +1,19 @@
+# The isBadVersion API is already defined for you.
+# def isBadVersion(version: int) -> bool:
+
+class Solution:
+    def firstBadVersion(self, n: int) -> int:
+        l = 1
+        r = n
+        result = 1
+        
+        while l <= r:
+            mid = (l + r) // 2
+            if isBadVersion(mid) == False:
+                l = mid + 1
+            else:
+                r = mid - 1
+                result = mid
+                
+        return result
+        
