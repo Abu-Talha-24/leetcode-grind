35e3707e836149c055297c61256891add7c296f4
Abu Talha <itsabutalha@gmail.com>
2023-02-26 19:23:31 +0530

    Time: 119 ms (46.63%), Space: 17.7 MB (68.81%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
@@ -8,23 +8,23 @@ class Solution:
         if not lists or len(lists) == 0:
             return None
         
-        while len(lists) > 1: # loop till all the lists are merged into one
+        while len(lists) > 1:
             mergedLists = []
-            
-            for i in range(0, len(lists), 2): # iterate through list in pairs
+        
+            for i in range(0, len(lists), 2):
                 l1 = lists[i]
                 l2 = lists[i+1] if (i+1) < len(lists) else None
-                mergedLists.append(self.mergeList(l1, l2)) # merge those pairs in one
-                
+                mergedLists.append(self.mergeLists(l1, l2))
+        
             lists = mergedLists
-        
+            
         return lists[0]
-        
-        
-    def mergeList(self, l1, l2):
+    
+    
+    def mergeLists(self, l1, l2):
         dummy = ListNode()
         tail = dummy
-        
+
         while l1 and l2:
             if l1.val < l2.val:
                 tail.next = l1
@@ -33,9 +33,16 @@ class Solution:
                 tail.next = l2
                 l2 = l2.next
             tail = tail.next
-        if l1:
+
+        while l1:
             tail.next = l1
-        if l2:
+            l1 = l1.next
+            tail = tail.next
+        while l2:
             tail.next = l2
-        
+            l2 = l2.next
+            tail = tail.next
+
         return dummy.next
+                
+                
