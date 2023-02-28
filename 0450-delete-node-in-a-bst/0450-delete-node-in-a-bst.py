c94679eb96ffb366143f288646b1703f417236ac
Abu Talha <itsabutalha@gmail.com>
2023-02-28 19:32:20 +0530

    Time: 78 ms (60.85%), Space: 18.4 MB (17.96%) - LeetHub

--- 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.py
+++ 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.py
@@ -5,33 +5,32 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def minValNode(self, root: Optional[TreeNode]):
+    def deleteNode(self, root: Optional[TreeNode], key: int) -> Optional[TreeNode]:
+        return self.delete(root, key)
+        
+    def minNode(self, root):
         curr = root
         while curr and curr.left:
             curr = curr.left
         return curr.val
     
-    def deleteNode(self, root: Optional[TreeNode], key: int) -> Optional[TreeNode]:
+    def delete(self, root, key):
         if not root:
             return root
         
         if key > root.val:
-            root.right = self.deleteNode(root.right, key)
+            root.right = self.delete(root.right, key)
         elif key < root.val:
-            root.left = self.deleteNode(root.left, key)
-        else:
-            if not root.left:
+            root.left = self.delete(root.left, key)
+        else: # if found node
+            if not root.left: 
                 return root.right
             elif not root.right:
                 return root.left
             else:
-                # minimum node from right subtree
-                minVal = self.minValNode(root.right) # the value to be replaced with
-                root.val = minVal # replace it
-                root.right = self.deleteNode(root.right, minVal) # delete the replaced node
-            
+                minVal = self.minNode(root.right) 
+                root.val = minVal
+                root.right = self.delete(root.right, minVal)
+        
         return root
-            
-            
-        
-        
+                
