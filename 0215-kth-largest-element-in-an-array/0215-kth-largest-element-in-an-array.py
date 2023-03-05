550137afd9f72b7283c779ed249235c7edc393d6
Abu Talha <itsabutalha@gmail.com>
2023-03-05 23:59:13 +0530

    Time: 558 ms (59.91%), Space: 27 MB (83.25%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.py
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.py
@@ -1,28 +1,14 @@
 class Solution:
     def findKthLargest(self, nums: List[int], k: int) -> int:
         
-        n = len(nums)
-        k = n - k  # calculating the index
+        minHeap = []
         
-        def quickSelect(l, r):
-            pivot = nums[r] # select pivot as rightmost element
-            p = l # pos starting from left
-            
-            # creating a partiion for <pivot and >pivot.
-            for i in range(l, r):
-                if nums[i] <= pivot: # if num is less than pivot
-                    nums[p], nums[i] = nums[i], nums[p] # swap with pos
-                    p += 1 
-                
-            # swap the pivot element with where pos last ended up at
-            nums[p], nums[r] = nums[r], nums[p] 
-            if p > k:
-                return quickSelect(l, p-1) # quickselect on left parition
-            elif p < k:
-                return quickSelect(p+1, r) # quickselect on right partitoin
-            else: # if p == k
-                return nums[p]
         
+        for num in nums:
+            heapq.heappush(minHeap, num)
             
-        return quickSelect(0, n - 1)
+            if len(minHeap) > k:
+                heapq.heappop(minHeap)
+        
+        return minHeap[0]
         
