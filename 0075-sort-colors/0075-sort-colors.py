c8570c248cf42e929d141e48c714db676a56ea4d
Abu Talha <itsabutalha@gmail.com>
2023-03-05 21:18:04 +0530

    Time: 37 ms (56.23%), Space: 13.8 MB (95.81%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -11,7 +11,6 @@ class Solution:
                 p += 1
         
         p = n-1
-        
         for i in range(n-1, -1, -1):
             if nums[i] > 1:
                 nums[p], nums[i] = nums[i], nums[p]
