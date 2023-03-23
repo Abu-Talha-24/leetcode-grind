a0b29608f8f6fc9814dcd29e56079b5dec484451
Abu Talha <itsabutalha@gmail.com>
2023-03-24 00:34:23 +0530

    Time: 41 ms (48.81%), Space: 13.8 MB (58.30%) - LeetHub

--- 1700-number-of-students-unable-to-eat-lunch/1700-number-of-students-unable-to-eat-lunch.py
+++ 1700-number-of-students-unable-to-eat-lunch/1700-number-of-students-unable-to-eat-lunch.py
@@ -2,15 +2,14 @@ class Solution:
     def countStudents(self, students: List[int], sandwiches: List[int]) -> int:
         
         
-        # rotate students queue:
         while students and sandwiches:
-        
+            if sandwiches[0] not in students:
+                return len(students)
+            
             if students[0] == sandwiches[0]:
                 students.pop(0)
                 sandwiches.pop(0)
-            elif sandwiches[0] not in students:
-                break
             else:
                 students.append(students.pop(0))
         
-        return len(students)
+        return 0
